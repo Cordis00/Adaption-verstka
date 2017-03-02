87e0a490e5180a8ed8faf0ff4f278f2d1efb79e0
Cordis00 <www.daka@mail.ru>
2017-03-02 17:16:38 +0200

    Error corection

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 var menu = document.getElementById('menu');
-var spisok = document.getElementById('spisok');
+var list = document.getElementById('list');
 var li = document.getElementsByTagName('li');
 
 menu.addEventListener('click', fun1);
@@ -7,14 +7,14 @@ for (var i = 0; i < li.length; i++) {
     li[i].addEventListener('click', fun2);
 
 }
-spisok.addEventListener('MouseOut', fun2);
+list.addEventListener('MouseOut', fun2);
 
 function fun1 () {
   menu.style.display = "none";
-  spisok.style.display = "block";
+  list.style.display = "block";
 }
 
 function fun2 () {
-  spisok.style.display = "none";
+  list.style.display = "none";
   menu.style.display = "block";
 }
